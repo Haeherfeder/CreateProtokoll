b9159580ec8487d1bf2a1d445d88d96d96173401
Haeherfeder <github@haeherfeder.de>
2019-03-01 12:32:04 +0100

    Changed Layout and add Input control.

--- src/de/haeherfeder/generate/protokoll/Main.java
+++ src/de/haeherfeder/generate/protokoll/Main.java
@@ -10,10 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			new Main();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} catch (IOException e) {e.printStackTrace();}
 	}
 	Main() throws IOException{
 		System.out.println("FileName: ");
@@ -24,26 +21,30 @@ public class Main {
 		FileWriter writer = null;
 		try {
 			writer = new FileWriter(file);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} catch (IOException e) {e.printStackTrace();}
 		System.out.println("Name: ");
 		String name = user_input.nextLine();
 		System.out.println("Klasse: ");
 		String klasse = user_input.nextLine();
+		System.out.println("Beisitzer: ");
+		String beisitzer = user_input.nextLine();
+		writer.write(name+"\t\t\t"+klasse+"\t\t\t"+"Beisitzer: "+beisitzer+"\n\n");
 		System.out.println("Thema: ");
 		String thema = user_input.nextLine();
-		writer.write(name+"\t\t\t"+klasse+"\t\t\t"+"Thema: "+thema+"\n\n");
+		writer.write("Thema: "+thema+"\n");
 		System.out.println("Aufgabenstellung: ");
 		String aufgabe = user_input.nextLine();
 		writer.write("Aufgabenstellung: "+aufgabe+"\n\n");
 		System.out.println("Hypothese: ");
 		String hypothese = user_input.nextLine();
-		writer.write("Hypothese: "+hypothese+"\n\n");
-		System.out.println("Anzahl der Materialen:  ");
-		writer.write("Materialen :\n");
-		int matanz = Integer.parseInt(user_input.nextLine());
+		writer.write("Hypothese: "+hypothese+"\n\nMaterialen:\n");
+		int matanz=-1;
+		while(matanz==-1) {
+			try {
+				System.out.println("Anzahl der Materialen:  ");
+				matanz = Integer.parseInt(user_input.nextLine());
+			} catch (NumberFormatException e) {e.printStackTrace();matanz=-1;}
+		}
 		for(int i=0;i<matanz;) {
 			i++;
 			System.out.println("Material "+i+":");
@@ -51,13 +52,48 @@ public class Main {
 			writer.write("\t"+material+"\n");
 		}
 		writer.write("\n");
+		matanz=-1;
+		while(matanz==-1) {
+			try {
+				System.out.println("Anzahl der Chemikalien:  ");
+				matanz = Integer.parseInt(user_input.nextLine());
+				if(matanz!=0) {
+					writer.write("\nChemikalien: \n");
+				}
+			} catch (NumberFormatException e) {e.printStackTrace();matanz=-1;}
+		}
+		for(int i=0;i<matanz;) {
+			i++;
+			System.out.println("Chemikalie "+i+":");
+			String chemikalien = user_input.nextLine();
+			writer.write("\t"+chemikalien+"\n");
+		}
+		writer.write("\n");
 		System.out.println("Zeilen für Skizze/Versuchsaufbau: ");
-		int freizeilen = Integer.parseInt(user_input.next());
-		writer.write("Skizze/Versuchsaufbau:"+zeilen(freizeilen+1)+"Tabelle:\n");
+		int freizeilen=-1;
+		while(freizeilen==-1) {
+			try {
+				freizeilen = Integer.parseInt(user_input.nextLine());
+			} catch (NumberFormatException e) {e.printStackTrace();freizeilen=-1;}
+		}
+		writer.write("Skizze/Versuchsaufbau:"+zeilen(freizeilen+2));
+		System.out.println("Durchführung: ");
+		String durchfuehrung = user_input.nextLine();
+		writer.write("Durchführung: "+durchfuehrung+"\n\n"+"Tabelle:\n");
 		System.out.println("Tabelle\nZeilen: ");
-		int zeilen = Integer.parseInt(user_input.nextLine());
+		int zeilen=-1;
+		while(zeilen==-1) {
+			try {
+				zeilen = Integer.parseInt(user_input.nextLine());
+			} catch (NumberFormatException e) {e.printStackTrace();zeilen=-1;}
+		}
 		System.out.println("Spalten: ");
-		int spalten = Integer.parseInt(user_input.nextLine());
+		int spalten = -1;
+		while(spalten==-1) {
+			try {
+				spalten = Integer.parseInt(user_input.nextLine());
+			} catch (NumberFormatException e) {e.printStackTrace();spalten=-1;}
+		}
 		for(int i=0;i<zeilen;i++) {
 			for(int i2=0;i2<spalten;i2++) {
 				System.out.println("Feld: "+i+"und"+i2);
@@ -66,13 +102,15 @@ public class Main {
 			}
 			writer.write("\n");
 		}
-		writer.write("\n");
 		System.out.println("Beobachtung: ");
 		String beobachtung = user_input.nextLine();
-		writer.write("Beobachtung: "+beobachtung+"\n\n");
+		writer.write("\nBeobachtung: "+beobachtung+"\n\n");
 		System.out.println("Auswertung: ");
 		String auswertung = user_input.nextLine();
 		writer.write("Auswertung: "+auswertung+"\n\n");
+		System.out.println("Fehleranalyse: ");
+		String fehleranalyse = user_input.nextLine();
+		writer.write("Fehleranalyse: "+fehleranalyse+"\n\n");
 		writer.flush();
 		writer.close();
 		user_input.close();
